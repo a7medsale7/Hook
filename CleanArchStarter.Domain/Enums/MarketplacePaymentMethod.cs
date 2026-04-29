d93cf8574a15cf8f91e82862f8c1582378af6d45
YousufZ10 <yusufelmnofy@gmail.com>
2026-04-29 06:06:44 +0200

    step1--enums,entities

---
+++ CleanArchStarter.Domain/Enums/MarketplacePaymentMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hook.Domain.Enums
+{
+    public enum MarketplacePaymentMethod
+    {
+        CashOnDelivery = 1,
+        CreditCard = 2,
+        OnlinePayment = 3
+    }
+}
---
+++ CleanArchStarter.Domain/Enums/MarketplaceProductCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hook.Domain.Enums
+{
+    public enum MarketplaceProductCategory
+    {
+        FishingRods = 1,
+        FishingReels = 2,
+        FishingLines = 3,
+        HooksAndRigs = 4,
+        LuresAndBaits = 5,
+        FishingAccessories = 6,
+        FishingClothing = 7,
+        SnorkelingAndDiving = 8,
+        BoatsAndMarineEquipment = 9,
+        StorageAndBags = 10
+    }
+}
